0c7bd7e24db104c5b68572b38c2fa90b117699a3
Christopher Su <christophersu9@gmail.com>
2015-09-17 20:59:38 -0700

    cli

--- cli.py
+++ cli.py
@@ -3,8 +3,8 @@ import click
 from encryptfs import EncryptFS
 
 @click.command()
-@click.argument('action', nargs=-1)
-@click.argument('password', nargs=-1)
+@click.argument('action', nargs=1)
+@click.argument('password', nargs=1)
 def main(action, password):
     encfs = EncryptFS(password)
     if action == 'encrypt':
