cb62e031f544e6cca9818462c914ae4b19276705
Christopher Su <christophersu9@gmail.com>
2016-02-21 16:21:59 -0800

    indentation

--- encryptfs/cli.py
+++ encryptfs/cli.py
@@ -9,18 +9,18 @@ from encryptfs import EncryptFS
 @click.argument('action', nargs=1)
 @click.argument('password', nargs=1)
 def main(action, password):
-    encfs = EncryptFS(password)
-    if action == 'encrypt':
-        encfs.encrypt_all()
-        click.echo("Encrypted all files.")
-    elif action == 'decrypt':
-        encfs.decrypt_all()
-        click.echo("Dencrypted all files.")
-    else:
-        click.echo("Unknown action.")
+  encfs = EncryptFS(password)
+  if action == 'encrypt':
+    encfs.encrypt_all()
+    click.echo("Encrypted all files.")
+  elif action == 'decrypt':
+    encfs.decrypt_all()
+    click.echo("Dencrypted all files.")
+  else:
+    click.echo("Unknown action.")
 
 def cli_entry():
-    main()
+  main()
 
 if __name__ == '__main__':
-    main()
+  main()
